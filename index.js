86717175d3732bdf5a723e407692ef5ae1a2cc67
MGAceret <mgaceret@gmail.com>
2025-11-21 01:58:42 +0800

    Imported Swaggerjsdocs, swagger ui express, & api versions 1 & 2

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi  from 'swagger-ui-express';
 import postRoutes from './src/routes/post.routes.js';
 import userRoutes from './src/routes/user.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
 import authRoutes from './src/routes/auth.routes.js';
 import photoRoutes from './src/routes/photo.routes.js';
+import v1PostRoutes from './src/api/v1/routes/post.routes.js';
+import v2PostRoutes from './src/api/v2/routes/post.routes.js';
 import { testConnection } from './src/config/db.js'; 
 import { errorHandler } from './src/middleware/errorHandler.middleware.js';
 
@@ -27,7 +31,7 @@ app.use(cors({
 
 const globalLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 5,                  // max 5 requests per IP per window
+  max: 100,                  // max 5 requests per IP per window
   message: "Too many requests, try again later."
 });
 
@@ -49,6 +53,23 @@ app.use('/api/users', userRoutes);
 app.use('/api', commentRoutes);
 // Mount the photo routes
 app.use('/api/photos', photoRoutes);
+
+// V1
+app.use('/api/v1/posts', v1PostRoutes);
+// V2
+app.use('/api/v2/posts', v2PostRoutes);
+
+// Swagger setup
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: { title: 'My API', version: '1.0.0' },
+  },
+  apis: ['./src/api/**/*.js'],  // read all route files in nested folders
+};
+
+const specs = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(errorHandler);
 
